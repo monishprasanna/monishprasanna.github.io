33dc7b803ab89e062ff5ec9c6800ce9eb3461e9e
Monish Prasanna S <143189813+monishprasanna@users.noreply.github.com>
2026-02-22 22:52:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,12 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './',
+    base: '/',
     plugins: [react(), tailwindcss()],
+    build: {
+      outDir: 'docs',
+      emptyOutDir: true,
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
     },
